8867cb810ff79fd411fc16b0a09e5914966149b9
sajadhussain <sakraan01@gmail.com>
2020-06-01 16:16:35 +0500

    Searchbar revision version

--- searchform.php
+++ searchform.php
@@ -3,8 +3,8 @@
 <div class="search-bar">
     <button class="search-btn" type="button"><img src="<?= get_template_directory_uri(); ?>/assets/images/search-btn.png" alt=""></button>
     <div class="search-box">
-        <form class="d-flex" target='<?= get_bloginfo( 'url' ); ?>'>
-            <input type="search" name="s" placeholder="Search Here...">
+        <form class="d-flex" action="/" method ="get">
+            <input type="search" name="s" placeholder="Search Here..." value="<?php the_search_query();?>">
             <button type="submit">Submit</button>
         </form>
     </div>
